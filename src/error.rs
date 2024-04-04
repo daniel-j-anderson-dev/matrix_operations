5b52c7edd3b62f9a1cbe55b849d544f3c1665b97
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-04-04 13:39:03 -0400

    Moved impl block

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,6 @@
+use std::ops::Neg;
+
+use num::Num;
 use thiserror::Error;
 
 use crate::Matrix;
@@ -21,6 +24,171 @@ pub enum MatrixError {
     
     #[error("Cannot create matrix because {0}")]
     DimensionError(#[from] DimensionError)
+}
+impl MatrixError {
+    /// Check if two matrices can be multiplied <br>
+    /// ## Parameters
+    /// - `lhs`: light hand side of a matrix product.
+    /// - `rhs`: right hand side of a matrix product.
+    /// ## Returns
+    /// - <b>UnitType `()`</b>
+    ///   - if `lhs` and `rhs` can be multiplied
+    /// ## Errors
+    /// - [MatrixError::Arithmetic]
+    ///   - if `lhs.width` != `rhs.height`
+    pub fn multiplication<E>(lhs: &Matrix<E>, rhs: &Matrix<E>) -> Result<(), Self> {
+        return if lhs.width() != rhs.height() {
+            Err(MatrixError::Arithmetic {
+                operation: MatrixArithmeticOperation::Multiplication,
+                dimension_error: DimensionError::LhsWidthNotEqualToRhsHeight {
+                    lhs_width: lhs.width(),
+                    rhs_height: rhs.height(),
+                },
+            })
+        } else {
+            Ok(())
+        };
+    }
+
+    /// Check if two matrices can be element-wise multiplied <br>
+    /// ## Parameters
+    /// - `lhs`: light hand side of an element-wise matrix product.
+    /// - `rhs`: right hand side of an element-wise matrix product.
+    /// ## Returns
+    /// - <b>UnitType `()`</b>
+    ///   - if `lhs` and `rhs` can be element-wise multiplied
+    /// ## Errors
+    /// - [MatrixError::Arithmetic]
+    ///   - if `lhs` and `rhs` have different dimensions
+    pub fn hadamard_product<E>(lhs: &Matrix<E>, rhs: &Matrix<E>) -> Result<(), Self> {
+        return if lhs.width() != rhs.width() || lhs.height() != rhs.height() {
+            Err(MatrixError::Arithmetic {
+                operation: MatrixArithmeticOperation::HadamardProduct,
+                dimension_error: DimensionError::DifferentDimensions {
+                    lhs_width: lhs.width(),
+                    lhs_height: lhs.height(),
+                    rhs_width: rhs.width(),
+                    rhs_height: rhs.height(),
+                },
+            })
+        } else {
+            Ok(())
+        };
+    }
+
+    /// Check if two matrices can be added <br>
+    /// ## Parameters
+    /// - `lhs`: light hand side of a matrix sum.
+    /// - `rhs`: right hand side of a matrix sum.
+    /// ## Returns
+    /// - <b>UnitType `()`</b>
+    ///   - if `lhs` and `rhs` can be added
+    /// ## Errors
+    /// - [MatrixError::Arithmetic]
+    ///   - if `lhs` and `rhs` have different dimensions
+    pub fn addition<E>(lhs: &Matrix<E>, rhs: &Matrix<E>) -> Result<(), Self> {
+        return if lhs.width() != rhs.width() || lhs.height() != rhs.height() {
+            Err(MatrixError::Arithmetic {
+                operation: MatrixArithmeticOperation::Addition,
+                dimension_error: DimensionError::DifferentDimensions {
+                    lhs_width: lhs.width(),
+                    lhs_height: lhs.height(),
+                    rhs_width: rhs.width(),
+                    rhs_height: rhs.height(),
+                },
+            })
+        } else {
+            Ok(())
+        };
+    }
+
+    /// Use this to check if a matrix, and index pair form a valid minor <br>
+    /// ## Parameters
+    /// - `matrix`: Matrix to take a minor from.
+    /// - `excluded_row_index`: the index of the row to leave out form this minor.
+    /// - `excluded_column_index`: the index of the column to leave out form this minor.
+    /// ## Returns
+    /// - <b>UnitType `()`</b>
+    ///   - if there exists a minor of `matrix` at the corresponding indexes
+    /// ## Errors
+    /// - [MatrixError::InvalidDimensions]
+    ///   - if `excluded_row_index` >= `matrix.height`
+    ///   - if `excluded_column_index` >= `matrix.width`
+    ///   - if `matrix` is not square
+    pub fn minor<E>(
+        matrix: &Matrix<E>,
+        excluded_row_index: usize,
+        excluded_column_index: usize,
+    ) -> Result<(), Self> {
+        return if excluded_row_index >= matrix.height() {
+            Err(DeterminantError::MinorError(MinorError::NoSuchRow(excluded_row_index)).into())
+        } else if excluded_column_index >= matrix.width() {
+            Err(
+                DeterminantError::MinorError(MinorError::NoSuchColumn(excluded_column_index))
+                    .into(),
+            )
+        } else if matrix.width() == 0 || matrix.height() == 0 {
+            Err(DeterminantError::DimensionError(DimensionError::Zero).into())
+        } else if matrix.width() < 2 || matrix.height() < 2 {
+            Err(DeterminantError::DimensionError(DimensionError::TooSmall).into())
+        } else if matrix.width() != matrix.height() {
+            Err(DeterminantError::DimensionError(DimensionError::NotSquare).into())
+        } else {
+            Ok(())
+        };
+    }
+
+    /// Use this to check if a matrix, and index pair form a valid minor <br>
+    /// ## Parameters
+    /// - `matrix`: Matrix to take the determinate of.
+    /// ## Returns
+    /// - <b>UnitType `()`</b>
+    ///   - if there exists a determinant of `matrix`
+    /// ## Errors
+    /// - [MatrixError::Determinant]
+    ///   - if `matrix.width` != `matrix.height`
+    ///   - if either dimension of `matrix` is `0`
+    pub fn determinant<E>(matrix: &Matrix<E>) -> Result<(), Self> {
+        return if matrix.width() == 0 || matrix.height() == 0 {
+            Err(MatrixError::Determinant(
+                DeterminantError::DimensionError(DimensionError::Zero),
+            ))
+        } else if matrix.width() != matrix.height() {
+            Err(MatrixError::Determinant(
+                DeterminantError::DimensionError(DimensionError::NotSquare),
+            ))
+        } else {
+            Ok(())
+        };
+    }
+
+    /// Use this to check if a matrix is invertible <br>
+    /// (in terms of matrix multiplication)
+    /// ## Parameters
+    /// - `matrix`: Matrix to invert.
+    /// ## Returns
+    /// - <b>UnitType `()`</b>
+    ///   - if there exists a multiplicative inverse of `matrix`
+    /// ## Errors
+    /// - [MatrixError::Inverse]
+    ///   - if either dimension of `matrix` is `0`
+    ///   - if `matrix` is not square
+    ///   - if the determinant of `matrix` is `0`
+    pub fn inverse<E: Num + Neg<Output = E> + Copy>(matrix: &Matrix<E>) -> Result<(), Self> {
+        return if matrix.width() == 0 || matrix.height() == 0 {
+            Err(MatrixError::Inverse(
+                InverseError::DimensionError(DimensionError::Zero),
+            ))
+        } else if matrix.width() != matrix.height() {
+            Err(MatrixError::Inverse(
+                InverseError::DimensionError(DimensionError::NotSquare),
+            ))
+        } else if matrix.determinant()?.is_zero() {
+            Err(MatrixError::Inverse(InverseError::DeterminantZero))
+        } else {
+            Ok(())
+        };
+    }
 }
 
 #[derive(Debug, Error)]
@@ -90,142 +258,4 @@ pub enum InverseError {
 
     #[error("{0}")]
     DimensionError(#[from] DimensionError),
-}
-
-impl MatrixError {
-    /// Check if two matrices can be multiplied <br>
-    /// ## Parameters
-    /// - `lhs`: light hand side of a matrix product.
-    /// - `rhs`: right hand side of a matrix product.
-    /// ## Returns
-    /// - <b>UnitType `()`</b>
-    ///   - if `lhs` and `rhs` can be multiplied
-    /// ## Errors
-    /// - [MatrixError::Arithmetic]
-    ///   - if `lhs.width` != `rhs.height`
-    pub fn multiplication<E>(lhs: &Matrix<E>, rhs: &Matrix<E>) -> Result<(), Self> {
-        return if lhs.width() != rhs.height() {
-            Err(MatrixError::Arithmetic {
-                operation: MatrixArithmeticOperation::Multiplication,
-                dimension_error: DimensionError::LhsWidthNotEqualToRhsHeight {
-                    lhs_width: lhs.width(),
-                    rhs_height: rhs.height(),
-                },
-            })
-        } else {
-            Ok(())
-        };
-    }
-
-    /// Check if two matrices can be element-wise multiplied <br>
-    /// ## Parameters
-    /// - `lhs`: light hand side of an element-wise matrix product.
-    /// - `rhs`: right hand side of an element-wise matrix product.
-    /// ## Returns
-    /// - <b>UnitType `()`</b>
-    ///   - if `lhs` and `rhs` can be element-wise multiplied
-    /// ## Errors
-    /// - [MatrixError::Arithmetic]
-    ///   - if `lhs` and `rhs` have different dimensions
-    pub fn hadamard_product<E>(lhs: &Matrix<E>, rhs: &Matrix<E>) -> Result<(), Self> {
-        return if lhs.width() != rhs.width() || lhs.height() != rhs.height() {
-            Err(MatrixError::Arithmetic {
-                operation: MatrixArithmeticOperation::HadamardProduct,
-                dimension_error: DimensionError::DifferentDimensions {
-                    lhs_width: lhs.width(),
-                    lhs_height: lhs.height(),
-                    rhs_width: rhs.width(),
-                    rhs_height: rhs.height(),
-                },
-            })
-        } else {
-            Ok(())
-        };
-    }
-
-    /// Check if two matrices can be added <br>
-    /// ## Parameters
-    /// - `lhs`: light hand side of a matrix sum.
-    /// - `rhs`: right hand side of a matrix sum.
-    /// ## Returns
-    /// - <b>UnitType `()`</b>
-    ///   - if `lhs` and `rhs` can be added
-    /// ## Errors
-    /// - [MatrixError::Arithmetic]
-    ///   - if `lhs.width` != `rhs.width`
-    ///   - if `lhs.height` != `rhs.height`
-    pub fn addition<E>(lhs: &Matrix<E>, rhs: &Matrix<E>) -> Result<(), Self> {
-        return if lhs.width() != rhs.width() || lhs.height() != rhs.height() {
-            Err(MatrixError::Arithmetic {
-                operation: MatrixArithmeticOperation::Addition,
-                dimension_error: DimensionError::DifferentDimensions {
-                    lhs_width: lhs.width(),
-                    lhs_height: lhs.height(),
-                    rhs_width: rhs.width(),
-                    rhs_height: rhs.height(),
-                },
-            })
-        } else {
-            Ok(())
-        };
-    }
-
-    /// Use this to check if a matrix, and index pair form a valid minor <br>
-    /// ## Parameters
-    /// - `matrix`: Matrix to take a minor from.
-    /// - `excluded_row_index`: the index of the row to leave out form this minor.
-    /// - `excluded_column_index`: the index of the column to leave out form this minor.
-    /// ## Returns
-    /// - <b>UnitType `()`</b>
-    ///   - if there exists a minor of `matrix` at the corresponding indexes
-    /// ## Errors
-    /// - [MatrixError::InvalidDimensions]
-    ///   - if `excluded_row_index` >= `matrix.height`
-    ///   - if `excluded_column_index` >= `matrix.width`
-    ///   - if `matrix.width` != `matrix.height`
-    pub fn minor<E>(
-        matrix: &Matrix<E>,
-        excluded_row_index: usize,
-        excluded_column_index: usize,
-    ) -> Result<(), Self> {
-        return if excluded_row_index >= matrix.height() {
-            Err(DeterminantError::MinorError(MinorError::NoSuchRow(excluded_row_index)).into())
-        } else if excluded_column_index >= matrix.width() {
-            Err(
-                DeterminantError::MinorError(MinorError::NoSuchColumn(excluded_column_index))
-                    .into(),
-            )
-        } else if matrix.width() == 0 || matrix.height() == 0 {
-            Err(DeterminantError::DimensionError(DimensionError::Zero).into())
-        } else if matrix.width() < 2 || matrix.height() < 2 {
-            Err(DeterminantError::DimensionError(DimensionError::TooSmall).into())
-        } else if matrix.width() != matrix.height() {
-            Err(DeterminantError::DimensionError(DimensionError::NotSquare).into())
-        } else {
-            Ok(())
-        };
-    }
-
-    /// Use this to check if a matrix, and index pair form a valid minor <br>
-    /// ## Parameters
-    /// - `matrix`: Matrix to take the determinate of.
-    /// ## Returns
-    /// - <b>UnitType `()`</b>
-    ///   - if there exists a determinant of `matrix`
-    /// ## Errors
-    /// - [MatrixError::NoDeterminant]
-    ///   - if `matrix.width` != `matrix.height`
-    pub fn determinant<E>(matrix: &Matrix<E>) -> Result<(), Self> {
-        return if matrix.width() == 0 || matrix.height() == 0 {
-            Err(MatrixError::Determinant(
-                DeterminantError::DimensionError(DimensionError::Zero),
-            ))
-        } else if matrix.width() != matrix.height() {
-            Err(MatrixError::Determinant(
-                DeterminantError::DimensionError(DimensionError::NotSquare),
-            ))
-        } else {
-            Ok(())
-        };
-    }
 }
