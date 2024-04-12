53c4da509541d5b465cdb24757eb444d7188491b
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-04-12 14:24:41 -0400

    Created modules for data sets and for regression

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
+pub mod data_set;
 pub mod error;
 pub mod matrix;
+pub mod regression;
 #[cfg(test)]
 pub mod test;
 
-pub use crate::error::*;
-pub use crate::matrix::*;
+pub use crate::{data_set::*, error::*, matrix::*, regression::*};
