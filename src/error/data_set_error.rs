a1c0a34d0a3cc60a2423a65245d7cdf22b1d4ab0
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-04-12 14:28:28 -0400

    Created data set error to support parsing from a text file

---
+++ src/error/data_set_error.rs
@@ -0,0 +1,64 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum DataSetError {
+    #[error("Failed to read dataset: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("Failed to parse dataset: {0}")]
+    Parse(#[from] ParseDataSetError),
+
+    #[error("Empty DataSet is not supported")]
+    Empty,
+}
+
+#[derive(Debug, Error)]
+#[error("Could not parse DataSet because {kind} on line {line_number}")]
+pub struct ParseDataSetError {
+    kind: ParseDataSetErrorKind,
+    line_number: usize,
+}
+impl ParseDataSetError {
+    pub fn missing_output(line_number: usize) -> Self {
+        return Self {
+            kind: ParseDataSetErrorKind::MissingOutput,
+            line_number,
+        };
+    }
+    pub fn missing_input(line_number: usize) -> Self {
+        return Self {
+            kind: ParseDataSetErrorKind::MissingInput,
+            line_number,
+        };
+    }
+    pub fn too_many_columns(line_number: usize) -> Self {
+        return Self {
+            kind: ParseDataSetErrorKind::TooManyColumns,
+            line_number,
+        };
+    }
+    pub fn parse_value_error<E: std::error::Error + 'static>(
+        line_number: usize,
+        parse_error: E,
+    ) -> Self {
+        return Self {
+            kind: ParseDataSetErrorKind::ParseValueError(parse_error.into()),
+            line_number,
+        };
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ParseDataSetErrorKind {
+    #[error("There is an output value missing")]
+    MissingOutput,
+
+    #[error("There is an input value missing")]
+    MissingInput,
+
+    #[error("There are too many columns for one (input, output) pair")]
+    TooManyColumns,
+
+    #[error("{0} is not a valid value")]
+    ParseValueError(#[from] Box<dyn std::error::Error>),
+}
